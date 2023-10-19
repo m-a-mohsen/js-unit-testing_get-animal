8a1b13f9e8ff7b5837e22cd6045f935eaedb63f8
m-a-mohsen <ma.abdel.mohsen@gmail.com>
2023-10-19 13:28:45 +0200

    The dog is eating my code

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-export function getAnimal() {}
+export function getAnimal(name) {
+  if (name === "cats") {
+    return `I totally love cats!`;
+  }
+  if (name && name !== "cats") {
+    return `I like ${name}!`;
+  }
+  if (!name) {
+    return `I do not like animals at all!`;
+  }
+}
